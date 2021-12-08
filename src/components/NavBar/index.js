bc01bd597c5b815249c85480cc813a0649581c4f
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-08 16:03:50 -0500

    added hamburger menu

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../../assets/images/blackIcon.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar } from "react-bootstrap";
 
 
 
@@ -13,23 +14,23 @@ function NavBar(props) {
     <a href="https://michellewehr.github.io/react-portfolio-2/">
         <img className="logo" src={Logo} alt="Michelle's logo"></img>
     </a>
-    <nav>
-    <ul className="nav nav-tabs row">
-      {tabs.map(tab => (
-        <li className="nav-item navItem col" key= {tab}>
-          <a
+    <Navbar collapseOnSelect expand="lg">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+               {tabs.map(tab => (
+            <Nav.Link className="nav-item navItem col"  style={{'font-size': "1.5rem"}} key= {tab}
             href={'#' + tab.toLowerCase()}
             onClick={() => props.handlePageChange(tab)}
             className={
               props.currentPage === tab ? 'nav-link active' : 'nav-link'
-            }
-          >
+            }>
             {tab}
-          </a>
-        </li>
+        </Nav.Link>
       ))}
-    </ul>
-    </nav>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     </header>
   );
   
